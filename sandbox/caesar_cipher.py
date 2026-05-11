bc9d6acb285a4e1f19d0cd71580b2f935767db5f
Manav Chaudhari <vrmanav02@gmail.com>
2026-05-12 00:15:24 +0530

    fix: make project iterable

--- sandbox/caesar_cipher.py
+++ sandbox/caesar_cipher.py
@@ -21,27 +21,30 @@ def caesar_cipher(message, operation, shift):
 
 
 def main():
-    subprocess.run("clear", shell=True)
-    message = input("Enter your message\n→ ")
-    operation = input("Do you want to encrypt or decrypt\n→ ").strip().lower()
-    while operation not in ("encrypt", "decrypt"):
-        print("⚠️ Invalid operation, choose either 'encrypt' or 'decrypt'")
-        operation = input("Do you want to encrypt or decrypt\n→ ").strip().lower()
     while True:
-        try:
-            shift = int(input("Enter shift value\n→ "))
-            break
-        except ValueError:
-            print("⚠️ Invalid shift value, type in a valid integer number")
-    result = caesar_cipher(message, operation, shift)
-    print(f"\n{operation.capitalize()}ed message is: {result} ")
-    play_again = input("\nWant to try another message? [yes/no]\n→ ").strip().lower()
-    while play_again not in ("yes", "no"):
+        subprocess.run(["clear"])
+        message = input("Enter your message\n→ ")
+        operation = input("Do you want to encrypt or decrypt\n→ ").strip().lower()
+        while operation not in ("encrypt", "decrypt"):
+            print("⚠️ Invalid operation, choose either 'encrypt' or 'decrypt'")
+            operation = input("Do you want to encrypt or decrypt\n→ ").strip().lower()
+        while True:
+            try:
+                shift = int(input("Enter shift value\n→ "))
+                break
+            except ValueError:
+                print("⚠️ Invalid shift value, type in a valid integer number")
+        result = caesar_cipher(message, operation, shift)
+        print(f"\n{operation.capitalize()}ed message is: {result} ")
         play_again = (
             input("\nWant to try another message? [yes/no]\n→ ").strip().lower()
         )
-    if play_again == "yes":
-        main()
+        while play_again not in ("yes", "no"):
+            play_again = (
+                input("\nWant to try another message? [yes/no]\n→ ").strip().lower()
+            )
+        if play_again == "no":
+            break
 
 
 if __name__ == "__main__":
