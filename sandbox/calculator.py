99768d5ff13704e26bf9fbed24a11483346734f2
Manav Chaudhari <vrmanav02@gmail.com>
2026-05-13 16:56:24 +0530

    Calculator App
    Add iterative logic & solve edge cases

--- sandbox/calculator.py
+++ sandbox/calculator.py
@@ -1,45 +1,65 @@
-def add(num1, num2):
-    return num1 + num2
+import subprocess
 
 
-def subtract(num1, num2):
-    return num1 - num2
+def take_number(message):
+    while True:
+        try:
+            num = float(input(message))
+            return num
+        except:
+            print("\t Invalid Input. Enter a numeric value")
 
 
-def multiply(num1, num2):
-    return num1 * num2
+def add(n1, n2):
+    return n1 + n2
 
 
-def divide(num1, num2):
-    return num1 / num2
+def subtract(n1, n2):
+    return n1 - n2
 
 
-def calculator():
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+def main():
+    subprocess.run(["clear"])
     operations = {
         "+": add,
         "-": subtract,
         "*": multiply,
         "/": divide,
     }
-    print("* WELCOME TO CALCULATOR *")
+    n1 = take_number("Enter first number: ")
     while True:
-        try:
-            num1 = float(input("Enter the first number\n: "))
-            num2 = float(input("Enter the first number\n: "))
-            break
-        except ValueError:
-            print("⚠️ Enter valid numbers\n")
-    print("\n", " ".join(operations))
-    operation = input("What operation would you like to perform\n: ")
-    while operation not in operations:
-        print("⚠️ Invalid operation")
-        operation = input("What operation would you like to perform\n: ")
-    if operations == "/" and num2 == 0:
-        print("⚠️ Cannot divide by zero")
-    else:
-        result = operations[operation](num1, num2)
-        print(f"\n{num1} {operation} {num2} = {result}")
+        n2 = take_number("Enter next number: ")
+        print("\nAvailable operations: ", ", ".join(operations.keys()))
+        operation = input("What operation would you like to perform: ").strip()
+        if n2 == 0 and operation == "/":
+            print("\tERROR - Divide by zero\n")
+            continue
+        while operation not in operations:
+            print("\tInvalid Input. Select from the operations listed above")
+            operation = input("What operation would you like to perform: ").strip()
+        result = operations[operation](n1, n2)
+        print(f"\n{n1} {operation} {n2} = {result}")
+        print("\n[C] Continue calculation\n[F] Fresh calculation\n[E] Exit")
+        do_continue = input("What would you like to do: ").strip().lower()
+        while do_continue not in ("c", "f", "e"):
+            print("\tInvalid choice. Select from the operations listed above.")
+            do_continue = input("What would you like to do: ").strip().lower()
+        if do_continue == "f":
+            main()
+        elif do_continue == "e":
+            return
+        elif do_continue == "c":
+            n1 = result
+            subprocess.run(["clear"])
 
 
 if __name__ == "__main__":
-    calculator()
+    main()
